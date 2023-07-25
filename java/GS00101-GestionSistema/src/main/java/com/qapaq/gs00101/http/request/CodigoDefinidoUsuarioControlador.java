9acb3a0e45f08ebbd1479338318975802041f3cd
Omar Velez <omargo33@gmail.com>
2023-07-25 00:44:00 -0500

    Up [Qq-Ov] -> Administrador

--- java/GS00101-GestionSistema/src/main/java/com/qapaq/gs00101/http/request/CodigoDefinidoUsuarioControlador.java
+++ java/GS00101-GestionSistema/src/main/java/com/qapaq/gs00101/http/request/CodigoDefinidoUsuarioControlador.java
@@ -1,10 +1,25 @@
 package com.qapaq.gs00101.http.request;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qapaq.gs00101.servicio.CodigoDefinidoUsuarioServicio;
 
 @RestController
 @RequestMapping(value = "/v1/codigosdefinidosusuarios")
 public class CodigoDefinidoUsuarioControlador {
-    
+
+    @Autowired
+    private CodigoDefinidoUsuarioServicio codigoDefinidoUsuarioServicio;
+
+    /**
+     * Metodo para mostrar todos los CodigosDefinidosUsuarios.
+     */
+    @GetMapping("/")
+    public String getAllCodigosDefinidosUsuarios() {
+        return codigoDefinidoUsuarioServicio.findAll();
+    }
+
 }
